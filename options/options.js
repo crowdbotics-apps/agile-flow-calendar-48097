03e46dedf6ea90eb0aec92edcad86be3d55aaf50
Crowdbotics <team@crowdbotics.com>
2024-05-24 21:14:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "agile-flow-calendar-48097",
+  url: "https://agile-flow-calendar-48097.botics.co",
+  api: "https://agile-flow-calendar-48097.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
